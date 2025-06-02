63ed4c4cf6f0111b39f860fb097a5d5a77d6dee6
Harshil Pathri <harshil@Harshils-MacBook-Air.local>
2025-06-02 15:29:26 -0700

    poker changes

--- poker.js
+++ poker.js
@@ -1,5 +1,5 @@
 const suits = ['S', 'H', 'D', 'C'];
-const values = ['A', 'K', 'Q', 'J', '1', '9', '8', '7', '6', '5', '4', '3', '2'];
+const values = ['A', 'K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2'];
 const selectedHand = [];
 const selectedBoard = [];
 const selectedCards = new Set();
@@ -14,7 +14,7 @@ function renderCards() {
             const cardCode = `${value}${suit}`;
             console.log(cardCode)
             const img = document.createElement('img');
-            img.src = `/images/cards/${cardCode}.png`;
+            img.src = `images/cards/${cardCode}.png`;
             img.alt = cardCode;
             img.className = 'card-img';
             img.dataset.card = cardCode;
@@ -30,7 +30,7 @@ function selectCard(card, imgElement) {
 
   if (selectedHand.length < 2) {
     selectedHand.push(card);
-    imgElement.classList.add('selected');
+    imgElement.classList.add('selected-hand');
     selectedCards.add(card);
   } else if (selectedBoard.length < 5) {
     selectedBoard.push(card);
@@ -42,8 +42,23 @@ function selectCard(card, imgElement) {
 }
 
 function updateDisplay() {
-  handDisplay.textContent = selectedHand.join(', ');
-  boardDisplay.textContent = selectedBoard.join(', ');
+    handDisplay.innerHTML = '';
+    boardDisplay.innerHTML = '';
+
+    selectedHand.forEach(card => {
+    const img = document.createElement('img');
+    img.src = `images/cards/${card}.png`;
+    img.alt = card;
+    handDisplay.appendChild(img);
+    });
+
+    selectedBoard.forEach(card => {
+    const img = document.createElement('img');
+    img.src = `images/cards/${card}.png`;
+    img.alt = card;
+    boardDisplay.appendChild(img);
+    });
+
 }
 
 function resetSelection() {
